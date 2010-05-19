af0edac5d470e68d1c5397800f50132f72ff2b7d
grosser <grosser.michael@gmail.com>
2010-05-19 21:35:26 +0200

    first resize...

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'environment'
 
 get "/magick" do
-  
+  image = MiniMagick::Image.from_blob(open(params[:url]).read)
+  image.resize params[:size]
+  image.to_blob
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,7 +11,9 @@ describe 'App' do
     Sinatra::Application.new
   end
 
-  it "xxx" do
-    get 'magick/xxx'
+  it "resizes" do
+    get 'magick?url=http%3A%2F%2Fgithub.com%2Fimages%2Fmodules%2Fheader%2Flogov3.png&size=200x'
+    File.open('/tmp/xxx.png', 'w'){|f| f.write last_response.body}
+    `identify /tmp/xxx.png`.should =~ /PNG 200x90 /
   end
 end
