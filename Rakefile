3eb09ea00f4ac1bdcc76feafc3dde938eed98a76
grosser <grosser.michael@gmail.com>
2010-05-20 09:44:35 +0200

    initialize newrelic and set correct environment

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 task :default => :spec
 begin
+  ENV['RACK_ENV'] = 'test'
   require 'spec/rake/spectask'
   Spec::Rake::SpecTask.new {|t| t.spec_opts = ['--color']}
 rescue LoadError
--- environment.rb
+++ environment.rb
@@ -6,5 +6,5 @@ Bundler.require
 require 'md5'
 
 if File.exist?('config/newreclic.yml')
-  NewRelic::Control.instance.init_plugin(:env => environment)
+  NewRelic::Control.instance.init_plugin(:env => Sinatra::Application.environment.to_s)
 end
