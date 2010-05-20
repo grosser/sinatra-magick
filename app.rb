1ed23eee7d13d8d593575a550d218bef796443d0
grosser <grosser.michael@gmail.com>
2010-05-20 08:08:44 +0200

    raise on invalid params

--- app.rb
+++ app.rb
@@ -10,6 +10,8 @@ class SinatraMagick
 end
 
 get "/magick" do
+  [:url, :size].each{|k| raise "i need #{k}" if params[k].to_s == '' }
+
   if SinatraMagick::SecretKeeper.secret != ''
     hash = MD5.md5('xxx' + params.reject{|k,v| k=='hash' }.sort.inspect)
     return "Hash does not match!" if hash != params[:hash].to_s
